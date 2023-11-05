f7bffbc044be66edf0d17846e8621816503e6575
Santosh Bhatt <santosbhatt15@gmail.com>
2023-11-06 00:05:13 +0530

    NAV BAR UPDATE: added the rescue, add post and profile fragments in the nav graph.

--- app/src/main/java/com/santoshbhatt/instapaw/presentation/ui/rescue/RescueViewModel.kt
+++ app/src/main/java/com/santoshbhatt/instapaw/presentation/ui/rescue/RescueViewModel.kt
@@ -1,13 +1,13 @@
-package com.santoshbhatt.instapaw.presentation.ui.dashboard
+package com.santoshbhatt.instapaw.presentation.ui.rescue
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class RescueViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is rescue Fragment"
     }
     val text: LiveData<String> = _text
 }
